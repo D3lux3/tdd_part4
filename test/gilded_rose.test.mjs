075af873718d710c610293e471aea1aa9403e086
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-29 16:46:02 +0300

    Added characterization tests

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -8,4 +8,42 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+
+
+  test("Item quality gets lowered by two", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 3)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).equal(1);
+  });
+
+  test("Item quality gets lowered by three if sell in is negative", () => {
+    const gildedRose = new Shop([new Item("foo", -1, 3)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).equal(1);
+  });
+
+  test("Item Aged Brie gets quality added by two", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 3)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).equal(5);
+  });
+
+  test("If Item is Aged Brie quality stays the same if the item quality more than 50", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 51)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).equal(51);
+  });
+
+  test("If Item is Backstage Pass and sell in is 0. Quality resets", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).equal(0);
+  });
+
+  test("If Item is Backstage Pass and sell in is 1. Quality increases by 3", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).equal(4);
+  });
+
 });
